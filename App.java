e7a8a253ed971fdaf4ddb751fab8777f5573f032
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-05-25 10:25:42 -0500

    complete exercise

--- App.java
+++ App.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 class App {
   int[] scores = { 72, 95, 91, 85, 87, 51, 65, 72, 75, 89 };
   int median;
+  int mean;
+  int highScore;
+  int lowScore;
 
   public int getMedian() {
     Arrays.sort(scores);
@@ -14,8 +17,29 @@ class App {
     return median;
   }
 
+  public int getMean() {
+    int sum = 0;
+    for (int i = 0; i < scores.length; i++) {
+      sum = sum + scores[i];
+    }
+    return sum / scores.length;
+  }
+
+  public int getLowestScore() {
+    Arrays.sort(scores);
+    return scores[0];
+  }
+
+  public int getHighestScore() {
+    Arrays.sort(scores);
+    return scores[scores.length - 1];
+  }
+
   public static void main(String args[]) {
     App app = new App();
-    System.out.println(app.getMedian());
+    System.out.println("The median is: " + app.getMedian());
+    System.out.println("The mean is: " + app.getMean());
+    System.out.println("The lowest score is: " + app.getLowestScore());
+    System.out.println("The highest score is: " + app.getHighestScore());
   }
 }
