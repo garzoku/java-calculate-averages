1fec340207dee409f2371fa78eb17512a56000c3
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-05-25 10:02:13 -0500

    print median

--- App.java
+++ App.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
+
 class App {
-  public static void main(String args[]){
+  int[] scores = { 72, 95, 91, 85, 87, 51, 65, 72, 75, 89 };
+  int median;
+
+  public int getMedian() {
+    Arrays.sort(scores);
+    if (scores.length % 2 == 0) {
+      median = (scores[scores.length / 2] + scores[scores.length / 2 - 1]) / 2;
+    } else {
+      median = scores[scores.length / 2];
+    }
+    return median;
+  }
+
+  public static void main(String args[]) {
+    App app = new App();
+    System.out.println(app.getMedian());
   }
 }
